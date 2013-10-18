995c8c667fef040b4f4e4f661bb28caa2d0796ff
Preeti Parasar <preeti@avninetworks.com>
2013-10-18 00:01:29 -0700

    <project name="python_proj1" default="compile">
      <taskdef resource="pyAntTasks.properties"/>
    
      <property name="src.dir" value="."/>
    
      <target name="compile">
        <py-compile dir="${src.dir}" pythonpath="${src.dir}" optimize="0"/>
      </target>
    	  <target name="run.feedparser" depends="compile">
    	    <py-run script="roman.py" pythonpath="${src.dir}" optimize="0">
    	      <arg value="http://www-106.ibm.com/developerworks/news/dw_dwtp.rss"/>
    	    </py-run>
    	  </target>
    </project>

--- build.sh
+++ build.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
 export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
+export LIB1=/eclipse/eclipse/plugins/org.apache.ant_1.8.4.v201303080030/lib
+
+export CLASSPATH=$CLASSPATH:$JAVA_HOME/lib/tools.jar:$LIB1/pyAntTasks-1.3.3.jar
+export PYTHONPATH=$PYTHONPATH:/usr/local/bin
 echo "calling the build script"
+
+echo "*****************************"
+echo "Calling a python script..."
+echo ""
 ws=`pwd`
 ${ws}/test.py
+echo ""
+echo "*****************************"
+echo ""
+
 ant -f Java_Proj1/build.xml
+ant -f python_proj1/build.xml
