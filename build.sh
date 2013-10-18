d81ad78a8309a4dd4aa7f1851606267600ec52c9
Preeti Parasar <preeti@avninetworks.com>
2013-10-18 00:52:49 -0700

    adding dependent lib

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+export ws=`pwd`
 export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
-export LIB1=/eclipse/eclipse/plugins/org.apache.ant_1.8.4.v201303080030/lib
+export LIB1=${ws}/lib
 
 export CLASSPATH=$CLASSPATH:$JAVA_HOME/lib/tools.jar:$LIB1/pyAntTasks-1.3.3.jar
 export PYTHONPATH=$PYTHONPATH:/usr/local/bin
@@ -9,7 +10,6 @@ echo "calling the build script"
 echo "*****************************"
 echo "Calling a python script..."
 echo ""
-ws=`pwd`
 ${ws}/test.py
 echo ""
 echo "*****************************"
