89830924f3bb4ae4456024621889a65c3ee02111
Preeti Parasar <preeti@avninetworks.com>
2013-10-20 18:06:48 -0700

    unit test

---
+++ unit-test.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+export ws=`pwd`
+export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
+export LIB1=${ws}/lib
+
+export CLASSPATH=$CLASSPATH:$JAVA_HOME/lib/tools.jar:$LIB1/pyAntTasks-1.3.3.jar
+export PYTHONPATH=$PYTHONPATH:/usr/local/bin
+
+py.test python_proj1/
+
