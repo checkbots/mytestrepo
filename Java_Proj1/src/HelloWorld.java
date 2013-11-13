35524224dec468bf2ef564b4e096f7a59c7c368f
avni-preeti <preeti@avninetworks.com>
2013-11-12 20:09:00 -0800

    Update HelloWorld.java

--- Java_Proj1/src/HelloWorld.java
+++ Java_Proj1/src/HelloWorld.java
@@ -1,10 +1,11 @@
-
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Wold from Java HelloWorls class");
 		System.out.println("Hello Wold from Java HelloWorls class");
+		System.out.println("Hello Wold from Java HelloWorls class");
+		
 
 	}
 
